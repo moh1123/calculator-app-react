f70fb810488ddbb567c38b9b927f59174c8dc0b0
Mohamad Allaham <mohamadallahampersonal@gmail.com>
2024-10-21 20:28:09 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='calc-grid'>
+      
+      <div className='output'>
+        <div className='previous-operand'>100</div>
+        <div className='current-operand'>522268*</div>
+      </div>
+
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>×</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+
+    </div>
+  )
+}
+
+export default App
